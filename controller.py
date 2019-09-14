1459106b2cfb8632f8d558ee7249449ee1dbf017
Michal Svoboda <svoboda.michal@seznam.cz>
2019-09-14 08:26:47 +0200

    scale test

---
+++ controller.py
@@ -0,0 +1,35 @@
+"""
+Main 
+"""
+
+import RPi_I2C_driver
+from time import *
+import serial
+
+
+
+
+lcd=RPi_I2C_driver.lcd()
+
+
+lcd.clear()
+lcd.lcd_display_string("Bienenprojekt",1)
+sleep(5)
+lcd.lcd_display_string("Starting!",2)
+sleep(5)
+
+
+con=serial.Serial()
+con.port="/dev/ttyUSB0"
+con.baurdrate=9600
+con.open()
+
+# read heading
+for i in range(7):
+    print(con.readline())
+
+for i in range(5):
+    print("reading:")
+    print(con.readline())
+
+con.close()
