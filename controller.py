f8b664af5126b0c9d7f7a0464b8bad8164275e81
Michal Svoboda <svoboda.michal@seznam.cz>
2019-09-14 09:24:45 +0200

    continuous test - temporary test version

--- controller.py
+++ controller.py
@@ -16,7 +16,6 @@ lcd.lcd_display_string("Bienenprojekt",1)
 sleep(1)
 lcd.lcd_display_string("Starting!",2)
 sleep(1)
-lcd.lcd_display_string("                ",2)
 
 con=serial.Serial()
 con.port="/dev/ttyUSB0"
@@ -27,11 +26,12 @@ con.open()
 for i in range(8):
     print(con.readline())
 
-for i in range(20):
+# retrieve and display temperature and weight readings
+while True:
     print("reading:")
     reading=con.readline().decode("utf-8").split(",")
     print(reading)
-    lcd_lcd_clear()
+    lcd.lcd_clear()
     lcd.lcd_display_string("Temp: "+reading[3],1)
     lcd.lcd_display_string("Weight:"+reading[1]+reading[2],2)
 
