5050f4869175e830d15b3b6e84b707430bdef74e
Michal Svoboda <svoboda.michal@seznam.cz>
2019-09-14 09:04:45 +0200

    temperature shown on lcd, bug fixes

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 """
-Main 
+Main
 """
 
 import RPi_I2C_driver
@@ -8,15 +8,14 @@ import serial
 
 
 
-
 lcd=RPi_I2C_driver.lcd()
 
 
 lcd.lcd_clear()
 lcd.lcd_display_string("Bienenprojekt",1)
-sleep(5)
+sleep(1)
 lcd.lcd_display_string("Starting!",2)
-sleep(5)
+sleep(1)
 lcd.lcd_display_string("                ",2)
 
 con=serial.Serial()
@@ -25,13 +24,17 @@ con.baurdrate=9600
 con.open()
 
 # read heading
-for i in range(7):
+for i in range(8):
     print(con.readline())
 
-for i in range(5):
+for i in range(20):
     print("reading:")
     reading=con.readline().decode("utf-8").split(",")
     print(reading)
-    lcd.lcd_display_string(reading[1]+reading[2],2)
+    lcd_lcd_clear()
+    lcd.lcd_display_string("Temp: "+reading[3],1)
+    lcd.lcd_display_string("Weight:"+reading[1]+reading[2],2)
 
 con.close()
+
+
