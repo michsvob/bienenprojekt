81288ace93d1363f1092891d10bedf6bec8e7db1
Michal Svoboda <svoboda.michal@seznam.cz>
2019-09-30 22:21:55 +0200

    small changes

--- BienenStation/BienenStation.ino
+++ BienenStation/BienenStation.ino
@@ -6,9 +6,9 @@
   Hardware:
   Microcontroller:
   - MKR FOX 1200 Arduino module with Sigfox functionality
-  
+
   Sensors
-  - Bosche Load Cell H40A 200 kg connected over Sparkfun OpenScale, which has an integrated temperature sensor 
+  - Bosche Load Cell H40A 200 kg connected over Sparkfun OpenScale, which has an integrated temperature sensor
   TMP 102
   https://www.bosche.eu/en/scale-components/load-cells/single-point-load-cell/single-point-load-cell-h40a
   http://www.ti.com/lit/ds/symlink/tmp102.pdf
@@ -28,7 +28,7 @@
 #include "DHT.h"
 
 // Set oneshot to false to trigger continuous mode when you finisched setting up the whole flow
-int oneshot = true;
+int oneshot = false;
 
 #define STATUS_OK     0
 #define STATUS_DHT_HUM_KO 1
@@ -39,7 +39,7 @@ int oneshot = true;
 #define STATUS_BMP_TMP_KO 32
 
 #define DHTPIN 2
-#define DHTTYPE DHT22 
+#define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 Adafruit_BMP280  bmp;
@@ -56,22 +56,27 @@ typedef struct __attribute__ ((packed)) sigfox_message {
   int8_t dhtTemperature;
   uint16_t dhtHumidity;
   uint16_t scaleWeight;
-  uint8_t scaleTemperature;
+  int8_t scaleTemperature;
   uint16_t bmpPressure;
-  uint8_t bmpTemperature;
+  int8_t bmpTemperature;
   uint8_t lastMessageStatus;
 } SigfoxMessage;
 
 // stub for message which will be sent
 SigfoxMessage msg;
 
+bool stringComplete;
+char inChar;
+String scaleReading;
+int messageCount = 0;
+
 void setup() {
 
-  if (oneshot == true) {
-    // Wait for the serial
-    Serial.begin(9600);
-    while (!Serial) {}
-  }
+  //if (oneshot == true) {
+  // Wait for the serial
+  Serial.begin(115200);
+  while (!Serial) {}
+  //}
 
   if (!SigFox.begin()) {
     // Something is really wrong, try rebooting
@@ -83,111 +88,125 @@ void setup() {
   //Send module to standby until we need to send a message
   SigFox.end();
 
-  if (oneshot == true) {
-    // Enable debug prints and LED indication if we are testing
-    SigFox.debug();
-  }
+  // Enable debug prints and LED indication if we are testing
+  SigFox.debug();
 
   // Configure the sensors and populate the status field
+  // DHT22
   dht.begin();
-  //if (!dht.begin()){
-  //  msg.status |= STATUS_DHT_HUM_KO;
-  //  msg.status |= STATUS_DHT_TMP_KO;
-  //} else {
-    if (oneshot == true) {
-      Serial.println("DHT OK");
-  //  }
-  }
+  Serial.println("DHT OK");
 
-  if (!bmp.begin()){
+  if (!bmp.begin()) {
     msg.status |= STATUS_BMP_PRS_KO;
     msg.status |= STATUS_BMP_TMP_KO;
-  } else {
-    if (oneshot == true) {
-       Serial.println("BMP OK");
-    }
+  }
+  else {
+    Serial.println("BMP OK");
   }
 
+  //BMP280
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  //OpenScale
+  //Readings from OpenScale are being sent over serial1
   Serial1.begin(9600);
-  
-  //if (!Serial1.begin(9600)){
-  //  msg.status |= STATUS_SCALE_WT_KO;
-  //  msg.status |= STATUS_SCALE_TMP_KO;
-  //} else {
-  
-  bool stringComplete=false;
-  char inChar;
-  String scaleReading;
-  
+  // Wait for Serial1
+  while (!Serial1) {
+    delay(50);
+    Serial.print("o");
+  }
+
   //skip headers sent by serial by OpenScale
-    for (int i=1; i<=8; i++){
-      delay(500);
-      while (Serial1.available() && stringComplete==false){
-        inChar=(char)Serial1.read();
-        scaleReading+=inChar;
-        if (inChar=='\n'){
-          stringComplete=true;
-          Serial.println(scaleReading);
-        }
+  for (int i = 1; i <= 8; i++) {
+    while (!Serial1.available()) {
+      delay(50);
+    }
+    stringComplete = false;
+    while (Serial1.available() && stringComplete == false) {
+      inChar = (char)Serial1.read();
+      scaleReading += inChar;
+      if (inChar == '\n') {
+        stringComplete = true;
+        Serial.println(scaleReading);
+        scaleReading="";
       }
     }
-    if (oneshot==true){
-      Serial.println("Scale OK");
-    }
   }
-//}
- 
-int messageCount=0;
+  Serial.println("Scale OK");
+}
 
 void loop() {
   // Every 15 minutes, read all the sensors and send data
-  // Let's try to optimize the data format
-  // Only use floats as intermediate representaion, don't send them directly
+  // Floats converted to bytes
 
-  messageCount=messageCount+1;
-  
+  messageCount++;
+  Serial.println("Message Nr.: " + String(messageCount));
+
+  //Readings
   //DHT22
   float dhtHumidity = dht.readHumidity();
-  msg.dhtHumidity = convertoFloatToUInt16(dhtHumidity, 100);
   float dhtTemperature = dht.readTemperature();
-  msg.dhtTemperature = convertoFloatToInt8(dhtTemperature, 60, -60);
 
   //BMP280
   float bmpPressure = bmp.readPressure();
-  msg.bmpPressure = convertoFloatToUInt16(bmpPressure, 200000);
   float bmpTemperature = bmp.readTemperature();
-  msg.bmpTemperature = convertoFloatToInt8(bmpTemperature, 60, -60);
 
   //OpenScale
-  String scaleReading;
+  String scaleReading = "";
   float scaleWeight;
   float scaleTemperature;
-  char inChar;
-  bool stringComplete = false;  // whether the string is complete
 
-  delay(2000);
-  while (Serial1.available() && stringComplete==false){
-    inChar=(char)Serial1.read();
-    scaleReading+=inChar;
-    if (inChar=='\n'){
-      stringComplete=true;
+  //Wait for message
+  //In case of timeout error, send a dummy message.
+  int timeOutCounter = 0;
+  stringComplete = false;
+
+  while (stringComplete == false) {
+    if (Serial1.available()) {
+      inChar = (char)Serial1.read();
+      scaleReading += inChar;
+      if (inChar == '\n') {
+        stringComplete = true;
+      }
+    }
+    else {
+      delay(50);
+      timeOutCounter++;
+      if (timeOutCounter > 1000) {
+        Serial.println("Timeout Error Reading from OpenScale.");
+        scaleReading = scaleReading + "0,0,0,0,0";
+        stringComplete = true;
+        break;
+      }
     }
   }
+
   Serial.println(scaleReading);//Po odladění smazat!
-  int ind1=scaleReading.indexOf(",");
-  int ind2=scaleReading.indexOf(",",ind1+1);
-  int ind3=scaleReading.indexOf(",",ind2+1);
-  int ind4=scaleReading.indexOf(",",ind3+1);
-  int ind5=scaleReading.indexOf(",",ind4+1);
+  int ind1 = scaleReading.indexOf(",");
+  int ind2 = scaleReading.indexOf(",", ind1 + 1);
+  int ind3 = scaleReading.indexOf(",", ind2 + 1);
+  int ind4 = scaleReading.indexOf(",", ind3 + 1);
   Serial.println(String(ind1));
   Serial.println(String(ind2));
   Serial.println(String(ind3));
   Serial.println(String(ind4));
-  scaleWeight=scaleReading.substring(ind1+1,ind2).toFloat();
-  msg.scaleWeight=convertoFloatToInt16(scaleWeight,-100,100);
-    
-  scaleTemperature=scaleReading.substring(ind3+1,ind4).toFloat();
-  msg.scaleTemperature=convertoFloatToInt8(scaleTemperature,-60,60);
+
+  scaleWeight = scaleReading.substring(ind1 + 1, ind2).toFloat();
+  scaleTemperature = scaleReading.substring(ind3 + 1, ind4).toFloat();
+
+
+  //Conversions
+  msg.dhtHumidity = convertoFloatToUInt16(dhtHumidity, 100);
+  msg.dhtTemperature = convertoFloatToInt8(dhtTemperature, 100, -100);
+  msg.bmpPressure = convertoFloatToUInt16(bmpPressure, 200000);
+  msg.bmpTemperature = convertoFloatToInt8(bmpTemperature, 100, -100);
+  msg.scaleWeight = convertoFloatToInt16(scaleWeight, 100, -100);
+  msg.scaleTemperature = convertoFloatToInt8(scaleTemperature, 100, -100);
 
   // Start the module
   SigFox.begin();
@@ -196,15 +215,14 @@ void loop() {
 
   // We can only read the module temperature before SigFox.end()
   float moduleTemperature = SigFox.internalTemperature();
-  msg.moduleTemperature = convertoFloatToInt8(moduleTemperature, 60, -60);
+  msg.moduleTemperature = convertoFloatToInt8(moduleTemperature, 100, -100);
 
-  if (oneshot==true){
-    Serial.println("Sigfox temperature: " + String(moduleTemperature));
-    Serial.println("DHT temperature: " + String(dhtTemperature));
-    Serial.println("DHT humidity: " + String(dhtHumidity));
-    Serial.println("Weight: " + String(scaleWeight));
-    Serial.println("OpenScale temperature: " + String(scaleTemperature));
-  }
+  Serial.println("Sigfox temperature: " + String(moduleTemperature));
+  Serial.println("DHT temperature: " + String(dhtTemperature));
+  Serial.println("DHT humidity: " + String(dhtHumidity));
+  Serial.println("Weight: " + String(scaleWeight));
+  Serial.println("OpenScale temperature: " + String(scaleTemperature));
+
   // Clears all pending interrupts
   SigFox.status();
   delay(1);
@@ -225,12 +243,6 @@ void loop() {
   //Sleep for 15 minutes
   LowPower.sleep(15 * 60 * 1000);
 }
-
-/*
-  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
-  routine is run between each time loop() runs, so using delay inside loop can
-  delay response. Multiple bytes of data may be available.
-*/
 
 void reboot() {
   NVIC_SystemReset();
